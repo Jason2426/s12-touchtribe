4aaf5e4eae437d579935c84b5e543e579ce74930
Jason <jay.jpy.wong@gmail.com>
2024-06-03 22:28:11 +0200

    Project files and folders setup

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('Hier komt je server voor Sprint 12.')
+// Import the npm package express from the node_modules map
+import express from 'express'
+
+// Import the fetchJson function from the ./helpers directory
+import fetchJson from './helpers/fetch-json.js';
+
+// Create a new express app
+const app = express();
+
+// Set ejs as the template engine
+app.set('view engine', 'ejs');
+
+// Make working with request data easier
+app.use(express.urlencoded({ extended: true }));
+
+// Set the directory for ejs templates
+app.set('views', './views');
+
+// Use the 'public' directory for static resources
+app.use(express.static('public'));
