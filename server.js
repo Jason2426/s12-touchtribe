0f125b4a2fd966ea93ac08890c28b80e77af4ee3
Jason <jay.jpy.wong@gmail.com>
2024-06-10 13:23:22 +0200

    Test

--- server.js
+++ server.js
@@ -21,14 +21,25 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 const productsURL = "https://cdn.contentful.com/spaces/x2maf5pkzgmb/entries/?access_token=VcJDwIe2eizDEjIwdVdDsF7tcQZ-0_uIrcP4BiDULsg&content_type=product&select=fields"
+const sizesURL = "https://cdn.contentful.com/spaces/x2maf5pkzgmb/environments/master/entries?access_token=VcJDwIe2eizDEjIwdVdDsF7tcQZ-0_uIrcP4BiDULsg&select=fields&content_type=size"
 
 app.get('/', function(request, response){
     Promise.all([
         fetchJson(`${productsURL}`)
     ]).then((productData) => {
         response.render('index', {products: productData})
+
     });
 })
+
+// app.get('/', function(request, response){
+//     Promise.all([
+//         fetchJson(`${productsURL}`, `${sizesURL}`)
+//     ]).then((productData, sizesData) => {
+//         response.render('index', {products: productData, sizes: sizesData})
+
+//     });
+// })
 
 // POST route for the index page
 app.post('/', function (request, response) {
