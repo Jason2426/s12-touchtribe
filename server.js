04b531a2b10be11cdd35348e7bea5a962eb1a12d
Jason <jay.jpy.wong@gmail.com>
2024-06-14 00:11:32 +0200

    Server refactor and test route for sizes

--- server.js
+++ server.js
@@ -1,57 +1,52 @@
-// Import the npm package express from the node_modules map
-
 import express from 'express';
-
-// Import the fetchJson function from the ./helpers directory
 import fetchJson from './helpers/fetch-json.js';
 
-// Create a new express app
 const app = express();
-
-// Set ejs as the template engine
 app.set('view engine', 'ejs');
 
-// Make working with request data easier
 app.use(express.urlencoded({ extended: true }));
-
-// Set the directory for ejs templates
 app.set('views', './views');
-
-// Use the 'public' directory for static resources
 app.use(express.static('public'));
+
+
+/* URL variables */ 
 
 const productsURL = "https://cdn.contentful.com/spaces/x2maf5pkzgmb/entries/?access_token=VcJDwIe2eizDEjIwdVdDsF7tcQZ-0_uIrcP4BiDULsg&content_type=product&select=fields"
 const sizesURL = "https://cdn.contentful.com/spaces/x2maf5pkzgmb/environments/master/entries?access_token=VcJDwIe2eizDEjIwdVdDsF7tcQZ-0_uIrcP4BiDULsg&select=fields&content_type=size"
+
+const configProdURL = "https://cdn.contentful.com/spaces/x2maf5pkzgmb/environments/master/entries?access_token=VcJDwIe2eizDEjIwdVdDsF7tcQZ-0_uIrcP4BiDULsg&content_type=configurableProduct"
+
+/* Different routes */
 
 app.get('/', function(request, response){
     Promise.all([
         fetchJson(`${productsURL}`)
     ]).then((productData) => {
         response.render('index', {products: productData})
-
     });
 })
 
-// app.get('/', function(request, response){
-//     Promise.all([
-//         fetchJson(`${productsURL}`, `${sizesURL}`)
-//     ]).then((productData, sizesData) => {
-//         response.render('index', {products: productData, sizes: sizesData})
+app.get('/test', function(request, response) {
+    Promise.all([
+        fetchJson(sizesURL),
+        fetchJson(productsURL),
+        fetchJson(configProdURL)
+    ]).then(([sizesData, productData, configData]) => {
+        response.render('test', { sizes: sizesData, products: productData, configs: configData  });
+        console.log(configData);
+    }).catch((error) => {
+        console.error('Error fetching data:', error);
+        response.status(500).send('Internal Server Error');
+    });
+});
 
-//     });
-// })
-
-// POST route for the index page
 app.post('/', function (request, response) {
     // Currently not handling POST data, redirect to the homepage
     response.redirect(303, '/');
 });
 
-// Set the port number for express to listen on
 app.set('port', process.env.PORT || 2024);
 
-// Start express and listen on the specified port : 2024
 app.listen(app.get('port'), function () {
-    // Log a message to the console with the port number
     console.log(`http://localhost:${app.get('port')}`);
 });
