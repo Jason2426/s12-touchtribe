8b3555d6ed77e61c06ba3495380321a8f2b313ff
Jason <jay.jpy.wong@gmail.com>
2024-06-15 22:28:05 +0200

    Server combined product fix

--- server.js
+++ server.js
@@ -15,15 +15,45 @@ const productsURL = "https://cdn.contentful.com/spaces/x2maf5pkzgmb/entries/?acc
 const sizesURL = "https://cdn.contentful.com/spaces/x2maf5pkzgmb/environments/master/entries?access_token=VcJDwIe2eizDEjIwdVdDsF7tcQZ-0_uIrcP4BiDULsg&select=fields&content_type=size"
 const configProdURL = "https://cdn.contentful.com/spaces/x2maf5pkzgmb/environments/master/entries?access_token=VcJDwIe2eizDEjIwdVdDsF7tcQZ-0_uIrcP4BiDULsg&content_type=configurableProduct"
 
-/* Different routes */
+const apiURL =
+  "https://cdn.contentful.com/spaces/x2maf5pkzgmb/environments/master/entries?access_token=VcJDwIe2eizDEjIwdVdDsF7tcQZ-0_uIrcP4BiDULsg&content_type=configurableProduct";
+const apiProductURL =
+  "https://cdn.contentful.com/spaces/x2maf5pkzgmb/environments/master/entries?access_token=VcJDwIe2eizDEjIwdVdDsF7tcQZ-0_uIrcP4BiDULsg&content_type=product";
+
+
+// Fetch the configurable products from Contentful
+const configurableProductsResponse = await fetchJson(apiURL);
+const configurableProducts = configurableProductsResponse.items; // Extract the items array from the response
+
+// Fetch the simple products from Contentful
+const simpleProductsResponse = await fetchJson(apiProductURL);
+const simpleProducts = simpleProductsResponse.items; // Extract the items array from the response
+
+/* Routes */
 
 app.get('/', function(request, response){
-    Promise.all([
-        fetchJson(`${productsURL}`)
-    ]).then((productData) => {
-        response.render('index', {products: productData})
+    // Combine configurable products with their corresponding simple products
+      const combinedProduct = configurableProducts.map((config) => {
+    
+        // Find the corresponding simple product
+        const simple = simpleProducts.find(
+          (simple) => simple.sys.id == config.fields.products[0].sys.id
+        );
+    
+        // Return an object combining information from both products
+        return {
+          id: config.sys.id,
+          name: config.fields.title,
+          image: simple.fields.primaryImage.sys,
+          size: simple.fields.size,
+          description: simple.fields.description,
+        };
+      });
+      console.log(combinedProduct);
+      response.render("index", {
+        products: combinedProduct, // Pass combinedProduct as 'products' to the EJS template
+      });
     });
-})
 
 app.get('/test', function(request, response) {
     Promise.all([
@@ -38,6 +68,7 @@ app.get('/test', function(request, response) {
         response.status(500).send('Internal Server Error');
     });
 });
+
 
 app.post('/', function (request, response) {
     // Currently not handling POST data, redirect to the homepage
