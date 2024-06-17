e6917c1ba6d95bc44408bda020af4deffd7f2b37
Jason <jay.jpy.wong@gmail.com>
2024-06-18 00:31:24 +0200

    Text change on browser lang preference

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,15 @@
-//
+
+// Check de voorkeurstaal 
+const userPreferredLanguage = navigator.language || navigator.userLanguage;
+const seeMoreProductsLabel = document.getElementById('see-more-products');
+let translatedText = seeMoreProductsLabel.getAttribute(`data-${userPreferredLanguage.substr(0, 2)}`);
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    console.log(userPreferredLanguage);
+    
+      // Werk de tekstinhoud bij als er een vertaling is
+      if (translatedText) {
+        seeMoreProductsLabel.textContent = translatedText;
+    }
+});
